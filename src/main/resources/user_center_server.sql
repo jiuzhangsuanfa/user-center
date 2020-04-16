55d5a22b189aa7112bf2d1cbfcf58ce3a759053f
plainyuan <mychenforeverl@gmail.com>
2020-04-16 18:00:53 +0800

    更新sql脚本，添加创库用库语句

--- src/main/resources/user_center_server.sql
+++ src/main/resources/user_center_server.sql
@@ -11,8 +11,11 @@
  Target Server Version : 80018
  File Encoding         : 65001
 
- Date: 16/04/2020 08:35:02
+ Date: 16/04/2020 17:55:20
 */
+
+create database user_center_server;
+use user_center_server;
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
@@ -21,8 +24,9 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- Table structure for address
 -- ----------------------------
 DROP TABLE IF EXISTS `address`;
-CREATE TABLE `address` (
-  `user_id` varchar(48) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT 'PK 用户id ',
+CREATE TABLE `address`
+(
+    `user_id`      varchar(48) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT 'PK 用户id ',
   `country` char(8) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '国家',
   `province` char(8) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '省',
   `city` char(8) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '市',
@@ -34,13 +38,14 @@ CREATE TABLE `address` (
 -- Table structure for article
 -- ----------------------------
 DROP TABLE IF EXISTS `article`;
-CREATE TABLE `article` (
-  `id` varchar(48) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '主键id',
-  `publish_time` datetime DEFAULT NULL COMMENT '发布时间',
-  `title` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '文章标题',
-  `content` blob COMMENT '文章内容',
-  `user_id` varchar(48) COLLATE utf8mb4_general_ci NOT NULL,
-  PRIMARY KEY (`id`) USING BTREE
+CREATE TABLE `article`
+(
+    `id`           varchar(48) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '主键id',
+    `publish_time` datetime                                                     DEFAULT NULL COMMENT '发布时间',
+    `title`        varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '文章标题',
+    `content`      text COLLATE utf8mb4_general_ci COMMENT '文章内容',
+    `user_id`      varchar(48) COLLATE utf8mb4_general_ci                       NOT NULL,
+    PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- ----------------------------
@@ -53,7 +58,9 @@ CREATE TABLE `article_tag`
     `tag_name`   varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '标签内容JSON',
     `id`         varchar(48) COLLATE utf8mb4_general_ci                       NOT NULL COMMENT '主键id',
     PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_general_ci;
 
 -- ----------------------------
 -- Table structure for register_record
@@ -83,7 +90,9 @@ CREATE TABLE `user`
     `phone_number` varchar(16) COLLATE utf8mb4_general_ci DEFAULT NULL,
     PRIMARY KEY (`id`) USING BTREE,
     KEY `user_username_index` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_general_ci;
 
 -- ----------------------------
 -- Table structure for user_preference
@@ -112,10 +121,12 @@ CREATE TABLE `user_profile` (
 -- Table structure for user_tag
 -- ----------------------------
 DROP TABLE IF EXISTS `user_tag`;
-CREATE TABLE `user_tag` (
-                            `user_id` varchar(48) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户id FK',
-                            `tag_name` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '标签名',
-                            PRIMARY KEY (`user_id`)
+CREATE TABLE `user_tag`
+(
+    `user_id`  varchar(48) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '用户id FK',
+    `tag_name` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '标签名',
+    `id`       varchar(255) COLLATE utf8mb4_general_ci                      NOT NULL,
+    PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 SET FOREIGN_KEY_CHECKS = 1;
