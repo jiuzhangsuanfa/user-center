1e6f10bb31d0e06ee7c33e68d5aa5603613a1225
Devin Don <i.inf@outlook.com>
2021-01-25 11:22:43 +0800

    chore(build): build script

---
+++ script/build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+mvn clean package -Dmaven.test.skip=true
+
+cd target
+pwd
+cat > Dockerfile <<EOF
+FROM openjdk:8-jre-slim
+
+COPY user-center-1.0.0.jar /app/index.jar
+
+ENTRYPOINT [ "java", "-jar", "/app/index.jar" ]
+EOF
+
+docker build -t $NAME .
+docker push $NAME
